6118098fd9350facc5bfb11b7e6f3286c84c0d5d
MemeDing <58700870+MemeDing@users.noreply.github.com>
2025-09-13 13:50:41 -0400

    Added a lot of stuff

--- Player.gd
+++ Player.gd
@@ -3,33 +3,71 @@ class_name Player
 
 var bullet_scene: PackedScene = preload("res://scenes/Bullet.tscn")
 
+# What device id to listen to (default 0 = keyboard)
+@export var device_id: int = 0
+
 # Maximum total velocity of the player (units/s)
-@export var max_velocity := 96
+@export var max_velocity: float = 128.0
 
 # Maximum total turn speed (deg/s)
-@export var max_turn_speed := 90
+@export var max_turn_speed: float = 90.0
 
 # Maximum health
-@export var max_health := 100
+@export var max_health: int = 100
 
-# Cooldown limit for bullets
-@export var bullet_cooldown_limit: float = 0.3
+# Current health
+var current_health: int = max_health
 
-# Current Health
-@export var current_health := max_health
+# Current shield
+var current_shield: int = 0
 
-# Cooldown timer for bullets.
-var _bullet_cooldown: float = 0.0
+# Fire rate (s)
+@export var fire_rate: float = 1.0
+
+# Fire rate modifier
+var fire_rate_modifier: float = 1.0
+
+# Gun cooldown Timer
+@onready var gun_cooldown_timer: Timer = Timer.new()
+
+func _ready() -> void:
+	gun_cooldown_timer.one_shot = true
+	add_child(gun_cooldown_timer)
 
 func _physics_process(delta: float) -> void:
 	handle_movement(delta)
 	handle_weapon_actions(delta)
 
+func handle_weapon_actions(delta: float) -> void:
+	# Fire Input
+	if Input.is_action_pressed("ui_primary_fire", device_id) and gun_cooldown_timer.is_stopped(): # Enter / Space
+		var bullet = bullet_scene.instantiate()
+		get_tree().current_scene.add_child(bullet)
+		bullet.fire(global_position, rotation, get_instance_id())
+		bullet.show()
+		gun_cooldown_timer.start(fire_rate * fire_rate_modifier)
+
+func add_shield(shield: int) -> void:
+	current_shield += shield
+
+func add_health(health: int) -> void:
+	current_health = min(current_health + health, max_health)
+	
+func apply_damage(damage: int) -> void:
+	if damage <= 0:
+		return
+	
+	var shield_before_damage = current_shield
+	current_shield = max(current_shield - damage, 0)
+	damage -= shield_before_damage - current_shield
+
+	current_health = max(current_health - damage, 0)
+
 func handle_movement(delta: float) -> void:
 	# Movement input: rotation
-	if Input.is_action_pressed("ui_left"): # A
+	if Input.is_action_pressed("ui_left", device_id): # A
 		rotation_degrees -= max_turn_speed * delta
-	if Input.is_action_pressed("ui_right"): # D
+	if Input.is_action_pressed("ui_right", device_id): # D
 		rotation_degrees += max_turn_speed * delta
 	
 	# Calculate new velocity
@@ -38,12 +76,3 @@ func handle_movement(delta: float) -> void:
 	# Apply movement
 	position += velocity * delta
 	#print("Position: ", position, " Velocity: ", velocity, " Heading: ", rotation_degrees)
-
-func handle_weapon_actions(delta: float) -> void:
-	_bullet_cooldown = max(0, _bullet_cooldown - delta)
-	if _bullet_cooldown == 0 and Input.is_action_pressed("ui_primary_fire"):
-		_bullet_cooldown = bullet_cooldown_limit
-		var bullet = bullet_scene.instantiate()
-		get_tree().current_scene.add_child(bullet)
-		bullet.fire(global_position, rotation, get_instance_id())
-		bullet.show()
