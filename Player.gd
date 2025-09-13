a3e4d14c6557e172841a3f35c6588479a99c932c
MemeDing <58700870+MemeDing@users.noreply.github.com>
2025-09-13 11:42:11 -0400

    Initial player

---
+++ Player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+class_name Player
+
+# Maximum total velocity of the player (units/s)
+@export var max_velocity := 2
+
+# Current velocity (units/s)
+# var velocity: Vector2
+
+# Current heading (deg)
+# var rotation_degrees: float
+
+# Maximum total turn speed (deg/s)
+@export var max_turn_speed := 60
+
+# Maximum health
+@export var max_health := 10
+
+# Current Health
+@export var current_health := max_health
+
+func _physics_process(delta: float) -> void:
+	handle_movement(delta)
+
+func handle_movement(delta: float) -> void:
+	# Movement input: rotation
+	if Input.is_action_pressed("ui_left"): # A
+		rotation_degrees -= max_turn_speed * delta
+	if Input.is_action_pressed("ui_right"): # D
+		rotation_degrees += max_turn_speed * delta
+	
+	# Calculate new velocity
+	var velocity = Vector2.from_angle(rotation) * max_velocity
+	
+	# Apply movement
+	position += velocity
+	#print("Position: ", position, " Velocity: ", velocity, " Heading: ", rotation_degrees)
