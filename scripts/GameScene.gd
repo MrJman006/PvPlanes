cf0f23f57378a9554c03fc2202c6231836e4f669
Tyler <tylerireland987@gmail.com>
2025-09-28 14:45:28 -0400

    fixed controller mapping

--- scripts/GameScene.gd
+++ scripts/GameScene.gd
@@ -7,9 +7,11 @@ func _ready() -> void:
 	spawn_players(players)
 
 func spawn_players(players: Array) -> void:
+	var device_id = 0
 	for i in range(players.size()):
 		if players[i] == true:
 			var new_player = player_scene.instantiate()
-			var random_pos = Vector2(randi_range(0,800),randi_range(0,800))
-			new_player.global_position = random_pos
+			new_player.set_device_id(device_id)
+			device_id += 1
 			add_child(new_player)
+			new_player.global_position = Vector2(randi_range(0,800),randi_range(0,800))
