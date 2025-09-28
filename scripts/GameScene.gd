1351cf30f3aebcbbbb7e0b6561f7d4cc202324ad
Tyler <tylerireland987@gmail.com>
2025-09-28 16:03:28 -0400

    somewhat at a working point

--- scripts/GameScene.gd
+++ scripts/GameScene.gd
@@ -7,11 +7,8 @@ func _ready() -> void:
 	spawn_players(players)
 
 func spawn_players(players: Array) -> void:
-	var device_id = 0
 	for i in range(players.size()):
-		if players[i] == true:
-			var new_player = player_scene.instantiate()
-			new_player.set_device_id(device_id)
-			device_id += 1
-			add_child(new_player)
-			new_player.global_position = Vector2(randi_range(0,800),randi_range(0,800))
+		var new_player = player_scene.instantiate()
+		new_player.set_device_id(players[i])
+		add_child(new_player)
+		new_player.global_position = Vector2(randi_range(0,800),randi_range(0,800))
