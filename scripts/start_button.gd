141e888a98d7559e2ff44e8491700d65ed012a2d
Tyler Ireland <111758942+tylerireland@users.noreply.github.com>
2025-09-28 13:55:54 -0400

    Menu to game transition (#11)
    * transition works now
    
    * fix PlayerMenu.tscn

--- scripts/start_button.gd
+++ scripts/start_button.gd
@@ -1,7 +1,4 @@
 extends TextureButton
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/players_menu.tscn")
-
-func _on_mouse_entered() -> void:
-	release_focus()
+	get_tree().change_scene_to_file("res://scenes/PlayerMenu.tscn")
